bf07396993033b933848406f39778861fff7f46c
Azashael <93047026+Azashael@users.noreply.github.com>
2021-11-10 22:08:24 +0100

    Bounce
    _ collision

--- Bouncy Slime/Assets/Scripts/Player/Slime.cs
+++ Bouncy Slime/Assets/Scripts/Player/Slime.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Slime : MonoBehaviour
 {
+    [Header("Animator")]
+    [SerializeField]
+    private Animator _animator;
+    [SerializeField]
+    private string _animationName;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,17 @@ public class Slime : MonoBehaviour
     void Update()
     {
 
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        this._animator.SetBool(this._animationName, true);
+        Debug.Log("Trigger exit");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        this._animator.SetBool(this._animationName, false);
+        Debug.Log("Trigger Enter");
     }
 }
