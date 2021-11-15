df7bb5e471b5b72fa1f2bb6437a253726d7947f6
Azashael <93047026+Azashael@users.noreply.github.com>
2021-11-15 22:13:38 +0100

    Ajout Slime
    _ ajout slime
    _ ring collision

--- Bouncy Slime/Assets/Scripts/Obstacle/RingManager.cs
+++ Bouncy Slime/Assets/Scripts/Obstacle/RingManager.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class RingManager : ObstacleManager
 {
-    [Header("Config")]
+    [SerializeField]
+    private GameObject[] _deactivate;
     [SerializeField]
     private bool _center;
 
     protected override void OnTriggerEnter(Collider other)
     {
-        if(_center)
+        foreach(GameObject gm in this._deactivate)
+        {
+            gm.SetActive(false);
+        }
+        if(this._center)
         {
             GameManager.instance.UpdateRingPoints();
         }
